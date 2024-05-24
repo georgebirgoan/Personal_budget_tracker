1e4adb021ba0513169c8abea5a7a17e165e8aab3
georgebirgoan <147092162+georgebirgoan@users.noreply.github.com>
2024-05-24 15:55:10 +0300

    changes server

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 // Configurare CORS
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"personal-budget-tracker-12.vercel.app",
     credentials: true,
 }));
 
