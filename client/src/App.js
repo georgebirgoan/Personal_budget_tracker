e91f7eeb5f1b51f97eb1f1aecae76b1f3b301a7e
georgebirgoan <147092162+georgebirgoan@users.noreply.github.com>
2024-06-14 23:37:38 +0300

    Loading page2

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-//protected routes for signup/in
 import {
   createBrowserRouter,
   RouterProvider,
@@ -7,101 +6,108 @@ import {
 import Navbar from "./components/navbar/Navbar";
 import Login from "./pages/login/Login";
 import SideBar from "./components/sidebar/SideBar";
-import SignUp from './pages/SignUp/SignUp'
-import Income from './pages/Income/Income'
-import Expenses from './pages/expenses/Expenses'
-import Footer from './pages/Footer/Footer'
-import './styles/global.scss'
+import SignUp from './pages/SignUp/SignUp';
+import Income from './pages/Income/Income';
+import Expenses from './pages/expenses/Expenses';
+import Footer from './pages/Footer/Footer';
+import './styles/global.scss';
 import EditIncome from './pages/EditIncome/EditIncome';
 import EditExpense from './pages/EditExpense/EditExpense';
-import PrivateRoutes from './pages/PrivateRoute/PrivateRoutes'
-import { useEffect } from "react";
-import { generateToken, messaging } from "./firebase";
-import { onMessage } from "firebase/messaging";
+import PrivateRoute from './pages/PrivateRoute/PrivateRoutes'; // Corect importul
 import ProfileDetails from "./pages/ProfileDetails/ProfileDetails";
 import Dasboard from "./pages/Dashboard/Dashboard";
- 
+
+const ProtectedLayout = () => {
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <SideBar />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+const PublicLayout = () => {
+  return (
+    <div className="main">
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <PublicLayout />,
+    children: [
+      {
+        path: "",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "/signup",
+    element: <PublicLayout />,
+    children: [
+      {
+        path: "",
+        element: <SignUp />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <PrivateRoute />, // Wrapper pentru rutele protejate
+    children: [
+      {
+        path: "/",
+        element: <ProtectedLayout />,
+        children: [
+          {
+            path: "",
+            element: <Dasboard />,
+          },
+          {
+            path: "income",
+            element: <Income />,
+          },
+          {
+            path: "expense",
+            element: <Expenses />,
+          },
+          {
+            path: "profile",
+            element: <ProfileDetails />,
+          },
+          {
+            path: "edit",
+            element: <Outlet />,
+            children: [
+              {
+                path: "income/:id",
+                element: <EditIncome />,
+              },
+              {
+                path: "expense/:id",
+                element: <EditExpense />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  const ProtectedLayout = () => {
-    return (
-      <div className="main">
-        <Navbar />
-        <div className="container">
-          <div className="menuContainer">
-            <SideBar />
-          </div>
-          <div className="contentContainer">
-            <Outlet />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  };
-  
-  const PublicLayout = () => {
-    return (
-      <div className="main">
-        <Outlet />
-      </div>
-    );
-  };
-  
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <PublicLayout />,
-      children: [
-        {
-          path: "/",
-          element: <PrivateRoutes />,
-          children: [
-            {
-              path: "/",
-              element: <Dasboard />,
-            },
-            {
-              path: "/income",
-              element: <Income />,
-            },
-            {
-              path: "/expense",
-              element: <Expenses />,
-            },
-            {
-              path: "/profile",
-              element: <ProfileDetails />
-            },
-            {
-              path: "/edit",
-              element: <Outlet />,
-              children: [
-                {
-                  path: "income/:id",
-                  element: <EditIncome />,
-                },
-                {
-                  path: "expense/:id",
-                  element: <EditExpense />,
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/signup",
-      element: <SignUp />,
-    },
-  ]);
-
   return <RouterProvider router={router} />;
 }
 
